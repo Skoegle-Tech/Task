ae3f1b316ee11644ff914f3f392801ec5b57de22
ManojGowda89 <mail@manojgowda.in>
2025-09-11 15:04:29 +0530

    djfh

--- client/src/pages/Anoncements.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Announcements() {
-  return (
-    <div>
-      <h1>Announcements</h1>
-    </div>
-  );
-}
--- client/src/utils/contants.js
+++
@@ -1,9 +0,0 @@
-const USERS_URL = "/user";
-
-const TASKS_URL = "/task";
-
-const ADMIN_URL = "/admin";
-
-const ANNOUNCEMENTS_URL = "/announcement";
-
-export { USERS_URL, TASKS_URL, ADMIN_URL, ANNOUNCEMENTS_URL };
